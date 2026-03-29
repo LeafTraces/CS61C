dfc90ae376c80566f5f0bba9f2d937a8734f8c6d
LeafTrace <1449070025@qq.com>
2026-03-29 20:20:15 +0800

    Lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,34 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+// Solution1
+// int ll_has_cycle(node *head) {
+//     /* your code here */
+//     node *tortoise = head;
+//     node *hare = head;
+
+//     while (hare != NULL && hare->next != NULL){
+//         hare = hare->next->next;
+//         tortoise = tortoise->next;
+
+//         if (hare == tortoise) return 1;
+//     }
+
+//     return 0;
+// }
+
+// Solution2
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node *tortoise = head;
+    node *hare = head;
+
+    do {
+        if (hare == NULL || hare->next == NULL) return 0;
+
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    } while (hare != tortoise);
+
+    return 1;
 }
