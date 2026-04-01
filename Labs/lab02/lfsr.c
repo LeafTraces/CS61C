c5970ce18a1d3eb0bbd01b2067bb933cca9fd60d
LeafTrace <88017173+LeafTraces@users.noreply.github.com>
2026-04-01 20:20:27 +0800

    lab2-first commit

--- Labs/lab02/lfsr.c
+++ Labs/lab02/lfsr.c
@@ -5,6 +5,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t bit0 = *reg & 1;
+    uint16_t bit2 = (*reg >> 2) & 1;
+    uint16_t bit3 = (*reg >> 3) & 1;
+    uint16_t bit5 = (*reg >> 5) & 1;
+    
+    uint16_t bit15 = bit0 ^ bit2 ^ bit3 ^ bit5;
+
+    *reg = (bit15 << 15) | (*reg >> 1);
 }
 
