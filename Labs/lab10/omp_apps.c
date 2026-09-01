6655b2fa0f306b0e967c08b8009dff8b0e0f7922
Shi <88017173+LeafTraces@users.noreply.github.com>
2026-09-02 01:29:50 +0800

    lab10 finished

--- Labs/lab10/omp_apps.c
+++ Labs/lab10/omp_apps.c
@@ -37,8 +37,11 @@ void v_add_optimized_adjacent(double* x, double* y, double* z) {
   // Do NOT use the `for` directive here!
   #pragma omp parallel
   {
-    for(int i=0; i<ARRAY_SIZE; i++)
+    int num_threads = omp_get_num_threads();
+    int start = omp_get_thread_num();
+    for(int i=start; i<ARRAY_SIZE; i += num_threads){
       z[i] = x[i] + y[i];
+    }
   }
 }
 
@@ -48,8 +51,17 @@ void v_add_optimized_chunks(double* x, double* y, double* z) {
   // Do NOT use the `for` directive here!
   #pragma omp parallel
   {
-    for(int i=0; i<ARRAY_SIZE; i++)
+    int num_threads = omp_get_num_threads();
+    int chunk_len = ARRAY_SIZE / num_threads;
+    int k = omp_get_thread_num();
+
+    int start = k * chunk_len;
+    int end = (k == num_threads - 1) ? ARRAY_SIZE : start + chunk_len;
+
+    for(int i = start; i < end; i++){
       z[i] = x[i] + y[i];
+    }
+      
   }
 }
 // END PART 1 EX 2
@@ -75,11 +87,17 @@ double dotp_manual_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
   #pragma omp parallel
   {
+    double acc = 0.0;
+
     #pragma omp for
-    for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
-      global_sum += x[i] * y[i];
+    for (int i = 0; i < arr_size; i++){
+      acc += x[i] * y[i];
+    }
+
+    #pragma omp critical
+    global_sum += acc;
   }
+
   return global_sum;
 }
 
@@ -88,12 +106,9 @@ double dotp_reduction_optimized(double* x, double* y, int arr_size) {
   // TODO: Modify this function
   // Please DO use the `reduction` directive here!
   double global_sum = 0.0;
-  #pragma omp parallel
-  {
-    #pragma omp for
-    for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
-      global_sum += x[i] * y[i];
+  #pragma omp parallel for reduction(+:global_sum)
+  for (int i = 0; i < arr_size; i++){
+    global_sum += x[i] * y[i];
   }
   return global_sum;
 }
--- Labs/lab10/server_utils.c
+++ Labs/lab10/server_utils.c
@@ -253,13 +253,11 @@ void serve_forever(int *socket_number) {
 #ifdef PROC
       // PART 2 TASK: Implement forking
       /* YOUR CODE HERE */
+      pid_t child_pid = fork();
 
-      if (/* YOUR CODE HERE */) {
+      if (child_pid == 0) {
          // This line kills the child process if parent dies
          int r = prctl(PR_SET_PDEATHSIG, SIGTERM);
-
-         /* YOUR CODE HERE */
-         
          // These lines exit the current process with code 1 
          // 1) when there was an error in prctl, 2) when the parent has been killed
          if (r == -1 || getppid() != parent_pid) {
@@ -268,6 +266,8 @@ void serve_forever(int *socket_number) {
          }
 
          /* YOUR CODE HERE */
+         dispatch(client_socket_number);
+         exit(0);
       }
 #else
       dispatch(client_socket_number);
